ec661f2a624bf806f962dbcca2bda575eea5f68c
kaushik.patel <kapatel@scan-it.com.sg>
2024-07-05 15:47:39 +0530

    added main file code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,22 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MessageQueue messageQueue = new MessageQueue();
+        Producer producer = new Producer(messageQueue);
+        Consumer consumer = new Consumer(messageQueue);
+
+        Thread producerThread = new Thread(producer);
+        Thread consumerThread = new Thread(consumer);
+        producerThread.start();
+        consumerThread.start();
+
+        try {
+            producerThread.join();
+            consumerThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Total messages processed successfully: " + consumer.getSuccessCount());
+        System.out.println("Total errors encountered: " + consumer.getErrorCount());
     }
 }
